03987d6ab63d9afed306a82098e6ff073b198c08
FullOfBologna <j.todd.atkins@gmail.com>
2015-07-15 20:21:37 -0600

    Added in TextView that says "Not Pressed"

--- app/src/main/java/com/example/appbrewery/targettracker/MainActivity.java
+++ app/src/main/java/com/example/appbrewery/targettracker/MainActivity.java
@@ -4,16 +4,23 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 
 public class MainActivity extends ActionBarActivity {
-
+    public TextView pressLabel;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        pressLabel = (TextView) findViewById(R.id.pressIndic);
+        pressLabel.setText("Not Pressed.");
 
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
