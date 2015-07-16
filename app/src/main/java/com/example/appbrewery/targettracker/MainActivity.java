029895b37a8f5ac924b605015221aa22519950a5
FullOfBologna <j.todd.atkins@gmail.com>
2015-07-16 17:54:44 -0600

    Added Target Graphic Removed Inital Text

--- app/src/main/java/com/example/appbrewery/targettracker/MainActivity.java
+++ app/src/main/java/com/example/appbrewery/targettracker/MainActivity.java
@@ -4,21 +4,28 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements View.OnTouchListener {
     public TextView pressLabel;
-    
+    public ImageView indSpot;
+    public float x,y;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        pressLabel = (TextView) findViewById(R.id.pressIndic);
-        pressLabel.setText("Not Pressed.");
+//        pressLabel = (TextView) findViewById(R.id.pressIndic);
+//        pressLabel.setText("Not Pressed.");
+        indSpot = (ImageView) findViewById(R.id.indicatorView);
 
     }
     @Override
@@ -27,6 +34,8 @@ public class MainActivity extends ActionBarActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+
+    
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -41,5 +50,17 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent ev){
+        if(ev.getAction() == ev.ACTION_DOWN) {
+            x = ev.getX();
+            y = ev.getY();
+            indSpot.layout((int)x,(int)y, 200, 200);
+            indSpot.setVisibility(View.VISIBLE);
+        }
+
+      return true;
     }
 }
